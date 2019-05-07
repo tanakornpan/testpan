a3945bf654f42701fbf98c4f58c5b817a8d20907
WORKSHOP\oastkk <tanakorn.khai@ku.th>
2019-05-07 14:36:55 +0700

    Updates

---
+++ precache-manifest.e095643729d72ad2725fdf8d6bd00ed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d823b11b101b11039d82425bf4f59712",
+    "url": "/testpan/index.html"
+  },
+  {
+    "revision": "d47f073d16374859d07e",
+    "url": "/testpan/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testpan/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d47f073d16374859d07e",
+    "url": "/testpan/static/js/main.20a76182.chunk.js"
+  },
+  {
+    "revision": "00dad615e72d1680d352",
+    "url": "/testpan/static/js/runtime~main.b0cd9f28.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/testpan/static/media/logo.ee7cd8ed.svg"
+  }
+]);
